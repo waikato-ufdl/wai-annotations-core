2aec8b20b7cb38a4de9868ef503b0c7f83b28f22
csterling <coreytsterling@gmail.com>
2021-05-20 13:42:48 +1200

    Added version bump to fix broken build.

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "wai",
         "wai.annotations"
     ],
-    version="0.1.0",
+    version="0.1.1",
     author='Corey Sterling',
     author_email='coreytsterling@gmail.com',
     install_requires=[
