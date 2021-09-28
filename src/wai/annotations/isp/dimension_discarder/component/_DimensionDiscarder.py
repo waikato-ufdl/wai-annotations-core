eabb9b7e7a911f2ca6065c33c82a25405780fbaf
Peter Reutemann <fracpete@gmail.com>
2021-09-28 14:35:39 +1300

    added --verbose flag to output information when annotations gets discarded

--- src/wai/annotations/isp/dimension_discarder/component/_DimensionDiscarder.py
+++ src/wai/annotations/isp/dimension_discarder/component/_DimensionDiscarder.py
@@ -1,5 +1,5 @@
 from wai.common.adams.imaging.locateobjects import LocatedObjects, LocatedObject
-from wai.common.cli.options import TypedOption
+from wai.common.cli.options import TypedOption, FlagOption
 
 from ....core.component import ProcessorComponent
 from ....core.stream import ThenFunction, DoneFunction
@@ -51,6 +51,11 @@ class DimensionDiscarder(
         help="the maximum area of annotations to convert"
     )
 
+    VERBOSE = FlagOption(
+        "--verbose",
+        help="outputs information when discarding annotations"
+    )
+
     def process_element(
             self,
             element: ImageObjectDetectionInstance,
@@ -75,20 +80,30 @@ class DimensionDiscarder(
         :return:                True if it should be discarded,
                                 False if it should be kept.
         """
+        log = self.VERBOSE is not None and self.VERBOSE
+
         # Min width check
         if self.MIN_WIDTH is not None and located_object.width < self.MIN_WIDTH:
+            if log:
+                self.logger.info("Width too small: %d < %d" % (located_object.width, self.MIN_WIDTH))
             return True
 
         # Max width check
         if self.MAX_WIDTH is not None and located_object.width > self.MAX_WIDTH:
+            if log:
+                self.logger.info("Width too large: %d > %d" % (located_object.width, self.MAX_WIDTH))
             return True
 
         # Min height check
         if self.MIN_HEIGHT is not None and located_object.height < self.MIN_HEIGHT:
+            if log:
+                self.logger.info("Height too small: %d < %d" % (located_object.height, self.MIN_HEIGHT))
             return True
 
         # Max height check
         if self.MAX_HEIGHT is not None and located_object.height > self.MAX_HEIGHT:
+            if log:
+                self.logger.info("Height too large: %d > %d" % (located_object.height, self.MAX_HEIGHT))
             return True
 
         # Return before calculating the area if there are no area bounds
@@ -104,10 +119,14 @@ class DimensionDiscarder(
 
         # Min area check
         if self.MIN_AREA is not None and area < self.MIN_AREA:
+            if log:
+                self.logger.info("Area too small: %d < %d" % (area, self.MIN_AREA))
             return True
 
-        # Min width check
+        # Max area check
         if self.MAX_AREA is not None and area > self.MAX_AREA:
+            if log:
+                self.logger.info("Area too large: %d > %d" % (area, self.MAX_AREA))
             return True
 
         return False
