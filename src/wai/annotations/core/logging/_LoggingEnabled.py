5ec8cbf0d235a094ae4b149428e5d881c8e7962d
Peter Reutemann <fracpete@gmail.com>
2022-07-12 09:36:08 +1200

    added note on WAIANN_MATPLOTLIB_LOGLEVEL env variable to adjust matplotlib's logging output

--- src/wai/annotations/core/logging/_LoggingEnabled.py
+++ src/wai/annotations/core/logging/_LoggingEnabled.py
@@ -2,13 +2,33 @@ import logging
 import os
 from typing import Optional
 
+
+def adjust_logging_level(logger_name, default_level, env_var):
+    """
+    Adjusts the logging level for the specified logger.
+
+    :param logger_name: the name of the logger, eg 'numba'
+    :type logger_name: str
+    :param default_level: the default logging level to use, if env var not set or value from env var fails to parse
+    :type default_level: int
+    :param env_var: the name of the environment variable
+    :type env_var: str
+    """
+    try:
+        logging.getLogger(logger_name).setLevel(level=os.getenv(env_var, default_level))
+    except:
+        logging.getLogger(logger_name).setLevel(level=default_level)
+
+
+# matplotlib outputs a lot of debugging info (fonts, etc)
+# to alleviate this, its logging level gets set to WARNING by default
+# can be changed via WAIANN_MATPLOTLIB_LOGLEVEL environment variable
+adjust_logging_level("matplotlib", logging.WARNING, "WAIANN_MATPLOTLIB_LOGLEVEL")
+
 # numba drowns out the output in the console with its debug messages
 # to alleviate this, its logging level gets set to WARNING by default
 # can be changed via WAIANN_NUMBA_LOGLEVEL environment variable
-try:
-    logging.getLogger('numba').setLevel(level=os.getenv("WAIANN_NUMBA_LOGLEVEL", logging.WARNING))
-except:
-    logging.getLogger('numba').setLevel(level=logging.WARNING)
+adjust_logging_level("numba", logging.WARNING, "WAIANN_NUMBA_LOGLEVEL")
 
 
 class LoggingEnabled:
