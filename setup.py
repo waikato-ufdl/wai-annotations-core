0b35e18752a0bce146e88484fbb7ef24b051316c
csterling <coreytsterling@gmail.com>
2022-12-16 09:55:14 +1300

    Updated wai.common requirement.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     author='Corey Sterling',
     author_email='coreytsterling@gmail.com',
     install_requires=[
-        "wai.common>=0.0.40",
+        "wai.common>=0.0.41",
         "wai.json>=0.0.4,<0.1",
         "wai.bynning>=0.0.2,<0.1",
         "Pillow",
